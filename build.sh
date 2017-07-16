66a8afd56e5bc35cba098037b2c7488b2a3b48ed
Greg Lincoln <greg@greglincoln.com>
2017-07-15 21:45:00 -0400

    Build/update script tweaks.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-${HOME}/opt/arduino-1.6.11/arduino --verify --board arduino:avr:mega:cpu=atmega2560 --pref build.path=${HOME}/code/printers/build/ Marlin/Marlin.ino
+/opt/arduino-1.8.3/arduino --verify --board arduino:avr:mega:cpu=atmega2560 --pref build.path=${HOME}/Code/printers/build/ Marlin/Marlin.ino
--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-avrdude -p atmega2560 -P /dev/ttyACM0 -b 115200 -c wiring -D -U "flash:w:marlin.hex:i"
+avrdude -p atmega2560 -P /dev/ttyACM0 -b 115200 -c wiring -D -U "flash:w:Marlin.ino.hex:i"
