727266fa18e35cb28fba0d9ab496b7862ad4d9a8
Greg Lincoln <greg@greglincoln.com>
2017-07-27 00:19:35 -0400

    Cleanups.

--- Configuration_ft5.h
+++ Configuration_ft5.h
@@ -271,6 +271,7 @@
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
+#define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_BED 1
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
